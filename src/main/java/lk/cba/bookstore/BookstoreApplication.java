7477daca5e0fda351e71fbee8719a4a4f9e322e7
supunuom <adsupun97@gmail.com>
2024-02-14 15:44:47 +0530

    added swagger support

--- src/main/java/lk/cba/bookstore/BookstoreApplication.java
+++ src/main/java/lk/cba/bookstore/BookstoreApplication.java
@@ -1,11 +1,20 @@
 package lk.cba.bookstore;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Book Store Backend",
+				version = "1.0.0",
+				description = "Backend end points."
+		)
+)
 public class BookstoreApplication {
 
 	public static void main(String[] args) {
